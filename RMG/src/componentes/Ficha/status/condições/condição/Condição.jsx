51e94cac474baca91f3af942da9fed7251c16471
Isaac <isaacmooca@hotmail.com>
2023-11-30 00:10:54 -0300

    Animações das condições melhoradas.

--- RMG/src/componentes/Ficha/status/condições/condição/Condição.jsx
+++ RMG/src/componentes/Ficha/status/condições/condição/Condição.jsx
@@ -5,14 +5,15 @@ const Condição = ({condição}) => {
   const [abertura, mudarAbertura] = useState(false)
 
   return (
-    <li>
+    <li id={condição.nome} className={abertura ? "aberto" : "fechado"}>
       <div className='condição'>
           <img src={condição.img}/>
           <a onClick={() => {mudarAbertura(a => !a)}}>{condição.nome}</a>
-          <div className="texto" id={condição.nome} style={{
+          <div className="texto" style={{
             maxHeight: abertura ? "400px" : 0,
-            padding: abertura ? "6px 6px 12px 6px" : 0,
-            transitionDelay: abertura ? "0s, 0s" : "0s, 0.5s"
+            paddingTop: abertura ? "6px" : 0,
+            paddingBottom: abertura ? "12px" : 0,
+            transitionDelay: abertura ? "0s, 0s" : "0s, 0s"
           }}>
               <p>{condição.efeito}</p>
               <span className="fonte">Fonte: {condição.fonte}</span>
